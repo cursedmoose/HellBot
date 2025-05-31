173ed8d198359e9cd62b6e32895876aacebfd274
cursedmoose <jim.duhala@gmail.com>
2025-05-31 02:57:27 -0700

    Add LocalVocal Support to OBS
    LocalVocal is an OBS plugin that runs a Whisper.cpp model for transcribing audio in real time. It outputs in SRT file format.

---
+++ OBS/LocalVocal.cs
@@ -0,0 +1,50 @@
+﻿namespace TwitchBot.OBS
+{
+    public class LocalVocal
+    {
+        bool Enabled = false;
+
+        string OutputPath = @"C:\Users\jimdu\Documents\Streaming\Transcriptions";
+        string ArchivePath = @"C:\Users\jimdu\Documents\Streaming\Transcriptions\Archive\";
+        string TranscriptPath = @"C:\Users\jimdu\Documents\Streaming\Transcriptions\desktop-output.srt";
+        string CommentaryPath = @"C:\Users\jimdu\Documents\Streaming\Transcriptions\mic-output.srt";
+
+        public LocalVocal(bool enabled)
+        {
+            Enabled = enabled;
+
+        }
+
+        public List<string> Archive()
+        {
+            return new()
+            {
+                ArchiveTranscript(),
+                ArchiveCommentary()
+            };
+        }
+
+        public string ArchiveTranscript()
+        {
+            return ArchiveFile(TranscriptPath);
+        }
+
+        public string ArchiveCommentary()
+        {
+            return ArchiveFile(CommentaryPath);
+        }
+
+        private string ArchiveFile(string filePath)
+        {
+            var currentArchivePath = Path.Combine(ArchivePath + $"{DateTime.Now:yyyy-MM-dd}");
+            Directory.CreateDirectory(currentArchivePath);
+            var numberOfFiles = Directory.GetFiles(currentArchivePath).Length;
+            var newFileName = Path.GetFileNameWithoutExtension(filePath) + $"-{numberOfFiles}" + Path.GetExtension(filePath);
+            var newFilePath = Path.Combine(currentArchivePath, newFileName);
+
+            File.Move(filePath, newFilePath);
+
+            return newFilePath;
+        }
+    }
+}
